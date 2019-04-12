7cd3a671e75c7b1bedc353ab7ceec8f2a09ff278
guillsav <guillsav@gmail.com>
2019-04-12 11:29:24 -0400

    Reducer done

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,56 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {ADD_SMURF, GET_SMURF, UPDATE_SMURF, DELETE_SMURF} from '../actions';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, action.payload],
+        addingSmurf: true,
+        fetchingSmurfs: false,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      };
+    case GET_SMURF:
+      return {
+        ...state,
+        smurfs: [],
+        fetchingSmurfs: true,
+        updatingSmurf: false,
+        deletingSmurf: false,
+        error: null
+      };
+    case UPDATE_SMURF:
+      return {
+        ...state,
+        smurfs: [...state.smurfs],
+        fetchingSmurfs: false,
+        updatingSmurf: true,
+        deletingSmurf: false,
+        error: null
+      };
+    case DELETE_SMURF:
+      return {
+        ...state,
+        smurfs: state.smurfs.filter(smurf => smurf.id !== action.payload),
+        fetchingSmurfs: false,
+        updatingSmurf: true,
+        deletingSmurf: true,
+        error: null
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
